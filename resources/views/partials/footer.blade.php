5d6b23db8c6e8fd331b928fdafd5485350cbc62e
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-02 00:22:41 +0100

    mainFooter

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,36 +4,7 @@
     @include('partials._topFooter')
 
     <!-- Main footer -->
-    <div class="main-footer">
-        <div class="container d-flex justify-content-between">
-
-            <!-- Footer Links -->
-            <div class="col-4 d-flex py-4">
-
-                <!-- First column: Site navigation & Shops Links -->
-                <div class="col-4">
-                    {{-- <LinkListComponent :listArray="dcNavLinks" /> --}}
-                    {{-- <LinkListComponent :listArray="dcShops" /> --}}
-                </div>
-
-                <!-- Second column: DC Legal Links -->
-                <div class="col-4">
-                    {{-- <LinkListComponent :listArray="dcLegals" /> --}}
-                </div>
-
-                <!-- Third column: DC Sites Links -->
-                <div class="col-4">
-                    {{-- <LinkListComponent :listArray="sitesList" /> --}}
-                </div>
-
-            </div>
-
-            <!-- Logo nel background -->
-            <div class="dc-footer-mark col-6">
-            </div>
-
-        </div>
-    </div>
+    @include('partials._mainFooter')
 
     <!-- BottomFooter Component Reference -->
     <BottomFooterComponent />
