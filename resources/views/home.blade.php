76b4d9cc2308255d909e531ac2859bbed398c535
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-02 00:01:16 +0100

    creazione _mainContent.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,23 +6,7 @@
         <div class="jumbotron"></div>
 
         <!-- Main content -->
-        <div class="main-content position-relative container">
-
-            <!-- Titolo: Current Series -->
-            <div class="group-tag position-absolute fs-1 pt-1">Current Series</div>
-
-            <!-- comicCard-container -->
-            <div class="container pt-5">
-                @include('partials._comicCard')
-            </div>
-
-            <div class="text-center py-3">
-                <button class="group-tag btn rounded-0 fs-5 py-1">
-                    Load more
-                </button>
-            </div>
-
-        </div>
+        @include('partials._mainContent')
 
     </main>
 @endsection
---
+++ resources/views/partials/_mainContent.blade.php
@@ -0,0 +1,17 @@
+<div class="main-content position-relative container">
+
+    <!-- Titolo: Current Series -->
+    <div class="group-tag position-absolute fs-1 pt-1">Current Series</div>
+
+    <!-- comicCard-container -->
+    <div class="container pt-5">
+        @include('partials._comicCard')
+    </div>
+
+    <div class="text-center py-3">
+        <button class="group-tag btn rounded-0 fs-5 py-1">
+            Load more
+        </button>
+    </div>
+
+</div>
