e323789321d5f90f478c0f8ad50796986d283428
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-01 23:48:37 +0100

    visualizzazione comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,32 @@
             <!-- Titolo: Current Series -->
             <div class="group-tag position-absolute fs-1 pt-1">Current Series</div>
 
-            <!-- Raccolta: Fumetti attuali -->
+            <!-- TODO: comicCard -->
             <div class="container pt-5">
-                ComicCardsComponentPlaceHolder
+                <div class="comic-container d-flex flex-wrap pt-3 g-0">
+                    @foreach ($comics as $comic)
+                        <div class="col-2">
+                            <div class="comic-thumb position-relative">
+
+                                <!-- Displays on hover -->
+                                <div
+                                    class="comic-info pe-2 position-absolute d-flex flex-column align-items-end justify-content-end">
+                                    <div class="comic-type text-capitalize">{{ $comic['type'] }}</div>
+                                    <div class="comic-price fs-5">{{ $comic['price'] }}</div>
+                                </div>
+
+                                <!-- Comic image -->
+                                <img class="img-fluid" src="{{ asset($comic['thumb']) }}" :alt="comic.series">
+
+                            </div>
+
+                            <!-- Comic Series display -->
+                            <div class="comic-series text-uppercase fs-5 py-3">
+                                {{ $comic['series'] }}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
 
             <div class="text-center py-3">
