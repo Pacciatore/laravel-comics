e10ab698fbd1304f0c16c1c8012879197f1d2e4f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-01 20:13:17 +0100

    creazione header no-style

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<?php
+$headerNavLi = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'];
+?>
+
+<header>
+
+    <div class="container">
+
+        <!-- Barra di navigazione -->
+        <nav class="navbar navbar-expand-lg justify-content-between">
+
+            <!-- Logo della pagina -->
+            <a class="img-container navbar-brand" href="#">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+            </a>
+
+            <!-- Elementi di navigazione -->
+            <ul class="navbar-nav">
+                <?php foreach ($headerNavLi as $navElement) {?>
+                <li class="nav-item item fs-5 px-1">
+                    <a href="#" class="nav-link">
+                        {{ $navElement }}
+                    </a>
+                </li>
+                <?php } ?>
+            </ul>
+
+        </nav>
+
+    </div>
+
+</header>
