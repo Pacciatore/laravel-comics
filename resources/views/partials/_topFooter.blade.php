11446cc3367ce126c816a7268c8a6354cb4ef6cc
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-02 00:17:54 +0100

    creazione e stile _topFooter

---
+++ resources/views/partials/_topFooter.blade.php
@@ -0,0 +1,53 @@
+    <!-- Top Footer -->
+    <div class="top-footer">
+
+        <div class="container d-flex py-5 justify-content-around align-items-center">
+
+            <!-- Digital Comics -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="{{ asset('/img/buy-comics-digital-comics.png') }}"
+                        alt="digital-comics">
+                    <span class="fs-5">Digital Comics</span>
+                </a>
+            </div>
+
+            <!-- DC Merchandise -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="{{ asset('/img/buy-comics-merchandise.png') }}"
+                        alt="comics-merchandise">
+                    <span class="fs-5">DC Merchandise</span>
+                </a>
+            </div>
+
+            <!-- Subscription -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="{{ asset('/img/buy-comics-subscriptions.png') }}"
+                        alt="subscriptions">
+                    <span class="fs-5">Subscription</span>
+                </a>
+            </div>
+
+            <!-- Comic Shop Locator -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon" src="{{ asset('/img/buy-comics-shop-locator.png') }}"
+                        alt="shop-locator">
+                    <span class="fs-5">Comic Shop Locator</span>
+                </a>
+            </div>
+
+            <!-- DC Power Visa -->
+            <div>
+                <a href="#">
+                    <img class="pre-footer-icon svg" src="{{ asset('/img/buy-dc-power-visa.svg') }}"
+                        alt="dc-power-visa">
+                    <span class="fs-5">DC Power Visa</span>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<footer class="fs-5 text-white">
+
+    <!-- TopFooter Component Reference -->
+    @include('partials._topFooter')
+
+    <!-- Main footer -->
+    <div class="main-footer">
+        <div class="container d-flex justify-content-between">
+
+            <!-- Footer Links -->
+            <div class="col-4 d-flex py-4">
+
+                <!-- First column: Site navigation & Shops Links -->
+                <div class="col-4">
+                    {{-- <LinkListComponent :listArray="dcNavLinks" /> --}}
+                    {{-- <LinkListComponent :listArray="dcShops" /> --}}
+                </div>
+
+                <!-- Second column: DC Legal Links -->
+                <div class="col-4">
+                    {{-- <LinkListComponent :listArray="dcLegals" /> --}}
+                </div>
+
+                <!-- Third column: DC Sites Links -->
+                <div class="col-4">
+                    {{-- <LinkListComponent :listArray="sitesList" /> --}}
+                </div>
+
+            </div>
+
+            <!-- Logo nel background -->
+            <div class="dc-footer-mark col-6">
+            </div>
+
+        </div>
+    </div>
+
+    <!-- BottomFooter Component Reference -->
+    <BottomFooterComponent />
+
+</footer>
