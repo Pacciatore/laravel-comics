5cecd26bb6e24c9d3041b1d3f68bb7db87803f67
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-01 20:12:50 +0100

    creazione home e layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="it">
+@extends('layouts.homeLayout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    {{-- Bootstrap cdnjs --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-    </script>
-
-    {{-- Link personal css --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    {{-- Link script js --}}
-    <script src="{{ asset('js/app.js') }}"></script>
-
-    <title>DC Comics</title>
-</head>
-
-<body>
-    <div class="container">
-        <div class="text-center">Hello</div>
-    </div>
-</body>
-
-</html>
+@section('content')
+@endsection
