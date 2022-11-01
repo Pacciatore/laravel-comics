e70dc409e78445136539b94730efe156576f8ca3
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-01 20:34:07 +0100

    content creation and styled

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.homeLayout')
 
 @section('content')
+    <main>
+
+        <div class="jumbotron"></div>
+
+        <!-- Main content -->
+        <div class="main-content position-relative container">
+
+            <!-- Titolo: Current Series -->
+            <div class="group-tag position-absolute fs-1 pt-1">Current Series</div>
+
+            <!-- Raccolta: Fumetti attuali -->
+            <div class="container pt-5">
+                ComicCardsComponentPlaceHolder
+            </div>
+
+            <div class="text-center py-3">
+                <button class="group-tag btn rounded-0 fs-5 py-1">
+                    Load more
+                </button>
+            </div>
+
+        </div>
+
+    </main>
 @endsection
