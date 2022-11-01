7faa53f66c39172c98f341729e5e0a85e481fa31
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-01 23:51:00 +0100

    creazione comicCard.blade.php

---
+++ resources/views/partials/_comicCard.blade.php
@@ -0,0 +1,23 @@
+<div class="comic-container d-flex flex-wrap pt-3 g-0">
+    @foreach ($comics as $comic)
+        <div class="col-2">
+            <div class="comic-thumb position-relative">
+
+                <!-- Displays on hover -->
+                <div class="comic-info pe-2 position-absolute d-flex flex-column align-items-end justify-content-end">
+                    <div class="comic-type text-capitalize">{{ $comic['type'] }}</div>
+                    <div class="comic-price fs-5">{{ $comic['price'] }}</div>
+                </div>
+
+                <!-- Comic image -->
+                <img class="img-fluid" src="{{ asset($comic['thumb']) }}" :alt="comic.series">
+
+            </div>
+
+            <!-- Comic Series display -->
+            <div class="comic-series text-uppercase fs-5 py-3">
+                {{ $comic['series'] }}
+            </div>
+        </div>
+    @endforeach
+</div>
