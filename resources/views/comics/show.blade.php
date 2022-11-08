14b7a6522695e9747f867542fa844b972625f78a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-08 02:26:56 +0100

    ordine container e classi bs

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.mainSectionLayout')
 
 @section('_mainContent')
-    <div class="comicShow">
+    <div class="comicShow position-relative">
+
+        <div class="blueRule"></div>
+
         <div class="position-relative container">
 
-            <div class="img-container text-center">
-                <img src="{{ asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
+            <div class="group-tag position-absolute fs-1 pt-1">
+                <div class="img-container text-center">
+                    <img src="{{ asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
+                </div>
             </div>
 
-            <div class="comic-info p-2">
+
+            <div class="comic-info">
                 <h1> {{ $comic['title'] }} </h1>
 
                 <p> {{ $comic['description'] }} </p>
