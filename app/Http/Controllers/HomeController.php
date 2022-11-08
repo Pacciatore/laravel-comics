2eb2606dcee683d7fac16cac195a0e09d53666fa
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-08 01:10:49 +0100

    creazione homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Comic;
+
 class HomeController extends Controller
 {
     //
     public function index()
     {
+
+        // $comics = Comic::all();
 
         $comics = config('data.comics');
         $siteLinks = config('data.siteLinks');
