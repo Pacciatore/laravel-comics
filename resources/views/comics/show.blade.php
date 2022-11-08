7099f743b5d43c312f1762c19d57c4ae104b5ffa
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-08 02:08:50 +0100

    cambio container in comic.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,19 +1,25 @@
 @extends('layouts.mainSectionLayout')
 
 @section('_mainContent')
-    <div class="img-container text-center">
-        <img src="{{ asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
-    </div>
+    <div class="comicShow">
+        <div class="comicShow position-relative container">
 
-    <div class="comic-info p-2">
-        <h1> {{ $comic['title'] }} </h1>
+            <div class="img-container text-center">
+                <img src="{{ asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
+            </div>
 
-        <p> {{ $comic['description'] }} </p>
+            <div class="comic-info p-2">
+                <h1> {{ $comic['title'] }} </h1>
 
-        <p> {{ $comic['price'] }} </p>
-    </div>
+                <p> {{ $comic['description'] }} </p>
 
-    <div class="btn btn-primary">
-        <a class="text-white text-decoration-none" href="{{ route('comic.index') }}">Back</a>
+                <p> {{ $comic['price'] }} </p>
+            </div>
+
+            <div class="btn btn-primary">
+                <a class="text-white text-decoration-none" href="{{ route('comic.index') }}">Back</a>
+            </div>
+
+        </div>
     </div>
 @endsection
