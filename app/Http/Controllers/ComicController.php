9fa109e51d9fac5765ec4ea52ac94f28dea879f5
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-14 14:11:20 +0100

    aggiunta validator e messaggio errore validate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -42,6 +42,14 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         //
+
+        $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'string|max:1500',
+            'thumb' => 'url|max:200',
+            'float' => 'numeric'
+        ]);
+
         $data = $request->all();
 
         $newComic = new Comic();
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,42 +12,84 @@
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="title">Titolo: </label>
                 <input type="text" name="title" id="title" value="{{ old('title') }}">
+                @error('title')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Descrizione fumetto --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="description">Descrizione: </label>
                 <input type="text" name="description" id="description" value="{{ old('description') }}">
+                @error('description')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Link thumb --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="thumb">Link immagine: </label>
                 <input type="url" name="thumb" id="thumb" value="{{ old('thumb') }}">
+                @error('thumb')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Prezzo fumetto --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="price">Prezzo: </label>
-                <input type="number" step=".01" name="price" id="price">
+                <input type="number" step=".01" name="price" id="price" value="{{ old('price') }}">
+                @error('price')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Serie del fumetto --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="series">Serie: </label>
-                <input type="text" name="series" id="series">
+                <input type="text" name="series" id="series" value="{{ old('series') }}">
+                @error('series')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Data di inizio vendita --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="sale_date">Data di vendita: </label>
-                <input type="date" name="sale_date" id="sale_date">
+                <input type="date" name="sale_date" id="sale_date" value="{{ old('sale_date') }}">
+                @error('sale_date')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Tipo fumetto --}}
             <div class="col-12 d-flex flex-column align-items-start justify-content-center">
                 <label for="type">Tipo: </label>
-                <input type="text" name="type" id="type">
+                <input type="text" name="type" id="type" value="{{ old('type') }}">
+                @error('type')
+                    <div class="alert alert-danger error-message">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
 
             {{-- Submit button --}}
